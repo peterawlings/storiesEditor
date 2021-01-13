4a54acae0c4f1729e9f0717a460e7e8f5a7cbc31
Pete Rawlings <rawlings.pete@gmail.com>
2021-01-14 09:25:41 +1100

    add delete and user stories

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
@@ -23,13 +24,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // Logging in
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev")); // Dev shows more info in console.
 }
 
 // Handlebars helpers
-const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs");
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
 
 // Handlebars
 app.engine(
@@ -40,6 +59,7 @@ app.engine(
       stripTags,
       truncate,
       editIcon,
+      select,
     },
     defaultLayout: "main",
     extname: ".hbs",
